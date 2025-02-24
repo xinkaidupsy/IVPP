c613e07248c86710a6ee76fd4f3fa4c2b0715f1a
Xinkai Du <xinkai.du.xd@gmail.com>
2025-02-24 20:37:05 +0800

    debugged prune_net

--- R/IVPP.R
+++ R/IVPP.R
@@ -134,12 +134,12 @@ IVPP_panelgvar <- function(data,
     stop("network to test should be either 'both', 'temporal', or 'contemporaneous'")
   }
 
-  if(partial_prune &
+  if(!partial_prune &
      prune_net %in% c("both", "temporal", "contemporaneous")){
     stop("prune_net should be either 'both', 'temporal', or 'contemporaneous'")
   }
 
-  if(partial_prune &
+  if(!partial_prune &
      prune_net != test){
     warning("The network you are partial pruning is different than the network you are testing equality on")
   }
@@ -584,12 +584,12 @@ IVPP_tsgvar <- function(data,
     stop("network to test should be either 'both', 'temporal', or 'contemporaneous'")
   }
 
-  if(partial_prune &
+  if(!partial_prune &
      prune_net %in% c("both", "temporal", "contemporaneous")){
     stop("prune_net should be either 'both', 'temporal', or 'contemporaneous'")
   }
 
-  if(partial_prune &
+  if(!partial_prune &
      prune_net != test){
     warning("The network you are partial pruning is different than the network you are testing equality on")
   }
